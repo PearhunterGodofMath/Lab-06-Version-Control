7ac2777be2628c39595dd65dc8cc1c21072dd08e
PearhunterGodofMath <dbcamp0419@gmail.com>
2024-10-22 22:50:07 -0400

    Test

--- encode.py
+++ encode.py
@@ -1,6 +1,11 @@
+# Team Member: David Campbell
+
 def encode(password):
-    encoded = "".join(str((int(digit) + 3) % 10) for digit in password)
-    return encoded
+    encoded_password = ""
+    for char in password:
+        encoded_digit = (int(char) + 3) % 10
+        encoded_password += str(encoded_digit)
+    return encoded_password
 
 
 def main():
@@ -15,12 +20,15 @@ def main():
 
         if option == "1":
             password = input("Please enter your password to encode: ")
-            encoded_password = encode(password)
-            print("Your password has been encoded and stored!")
+            if len(password) == 8 and password.isdigit():
+                encoded_password = encode(password)
+                print("Your password has been encoded and stored!")
+            else:
+                print("Invalid password. Please enter an 8-digit number.")
 
         elif option == "2":
-            original_password = decode(encoded_password)
-            print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+            # original_password = decode(encoded_password)
+            # print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
 
         elif option == "3":
             break
