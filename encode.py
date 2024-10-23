f67398f72a9744be3a8089f8e2b2eccb5939f34c
PearhunterGodofMath <dbcamp0419@gmail.com>
2024-10-23 11:00:50 -0400

    Update encode.py

--- encode.py
+++ encode.py
@@ -3,8 +3,8 @@
 def encode(password):
     encoded_password = ""
     for char in password:
-        encoded_digit = (int(char) + 3) % 10
-        encoded_password += str(encoded_digit)
+        encoded_digit = (int(char) + 3) % 10 # Shifts each digit up by 3.
+        encoded_password += str(encoded_digit) # Puts all the encoded digits in order.
     return encoded_password
 
 def decode(password):
@@ -14,26 +14,26 @@ def decode(password):
     return str(string)
 
 def main():
-    while True:
+    while True: # Prints Menu Options.
         print("\nMenu")
         print("-------------")
         print("1. Encode")
         print("2. Decode")
         print("3. Quit\n")
 
-        option = input("Please enter an option: ")
+        option = input("Please enter an option: ") # User chooses an option.
 
-        if option == "1":
+        if option == "1": # Encodes user inputted password.
             password = input("Please enter your password to encode: ")
-            if len(password) == 8 and password.isdigit():
+            if len(password) == 8 and password.isdigit(): # Needs to be an 8-digit password.
                 encoded_password = encode(password)
                 print("Your password has been encoded and stored!")
-            else:
+            else: # Not 8-digits.
                 print("Invalid password. Enter an 8-digit number.")
 
-        elif option == "2":
+        elif option == "2": # Uses the encoded password and decodes it.
             original_password = decode(encoded_password)
             print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
 
-        elif option == "3":
+        elif option == "3": # Ends the program.
             break
